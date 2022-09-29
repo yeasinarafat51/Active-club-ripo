9f8498594a1a90530d98e79f80e8323cae36f6f5
yeasinarafat51 <aloondokar65@gmail.com>
2022-09-29 18:38:54 +0600

    second commit

--- src/Components/Question/Question.js
+++ src/Components/Question/Question.js
@@ -8,7 +8,7 @@ const Question = () => {
             <h1>props and state differance</h1>
             <p>Props are used to pass data from one component to another. The state is a local data storage that is local to the component only and cannot be passed to other components. The this. setState property is used to update the state values in the component.</p>
             <h1>useEffect dataload any other work</h1>
-            <p>The useEffect Hook Usages. The callback function we pass a to the useEffect hook runs the side effects. React runs it on every render of a component by default.</p>
+            <p>The useEffect Hook Usages. The callback function we pass a to also useEffect hook runs the side effects. React runs it on every render of a component by default.</p>
         </div>
     );
 };
