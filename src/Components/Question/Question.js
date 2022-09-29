b425f9a95b1405c87ce2cde517f79fa170fb1add
yeasinarafat51 <aloondokar65@gmail.com>
2022-09-29 18:34:35 +0600

    second commit

--- src/Components/Question/Question.js
+++ src/Components/Question/Question.js
@@ -4,7 +4,7 @@ const Question = () => {
     return (
         <div>
             <h1>how does react work?</h1>
-            <p>ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code</p>
+            <p>ReactJS divides the UI into isolated reusable pieces of the code known as acomponents. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code</p>
             <h1>props and state differance</h1>
             <p>Props are used to pass data from one component to another. The state is a local data storage that is local to the component only and cannot be passed to other components. The this. setState property is used to update the state values in the component.</p>
             <h1>useEffect dataload any other work</h1>
